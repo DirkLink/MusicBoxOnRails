7f35eec2a1a1cddd482899b8bc47c595398f1fce
Dirk Link <dlink87@MacBook-Pro.local>
2015-07-01 15:08:31 -0400

    fixed all bugs.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:spotify]
 
+  before_create do
+    self.votes_left = 10
+  end
   has_many :votes
   has_many :songs
 
