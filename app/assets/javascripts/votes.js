4d98988c136601533f59a29f2e00a0977a35a205
Dirk Link <dlink87@MacBook-Pro.local>
2015-07-02 09:53:30 -0400

    Extracted the vote_function in JS and fixed the out of votes error message

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,35 +1,23 @@
+  var vote_function = function() {
+    var votes = $(this).parent().children(".voter")
+     console.log($(this).data())
+     console.log(this)
+     window.jd = this
+      $.ajax("/vote", {
+          method: "POST",
+          data: {
+            song_title: $(this).data().songTitle,
+            value: $(this).data().voteValue 
+          },
+          success: function(data) {
+            votes.text(data.total_votes)
+            if (typeof data.error_message != 'undefined') {
+              alert(data.error_message)
+            } 
+          }
+       })
+  }
 $(document).on("ready", function(){
-  $(".upvote").on("click", function() {
-    var votes = $(this).parent().children(".voter")
-    $.ajax("/vote", {
-        method: "POST",
-        data: {
-          song_title: $(this).data().songTitle,
-          value: $(this).data().voteValue 
-        },
-        error: function(data) {
-          alert(data.error_message)
-        },
-        success: function(data) {
-          votes.text(data.total_votes) 
-        }
-     })
-  })
-
-  $(".downvote").on("click", function() {
-    var votes = $(this).parent().children(".voter")
-    $.ajax("/vote", {
-        method: "POST",
-        data: {
-          song_title: $(this).data().songTitle,
-          value: $(this).data().voteValue 
-        },
-        error: function(data) {
-          alert(data.error_message)
-        },
-        success: function(data) {
-          votes.text(data.total_votes) 
-        }
-     })
-  })
+  $(".upvote").on("click", vote_function)
+  $(".downvote").on("click", vote_function)
 })
